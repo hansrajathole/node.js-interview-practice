ae1d2e3cda23f20f6c5f0d5fe24bb8cbd26bb3df
hansrajathole <hansrajathole1@gmail.com>
2025-06-08 23:57:05 +0530

    userSchema createdd

--- CRUD Operation/src/models/user.model.js
+++ CRUD Operation/src/models/user.model.js
@@ -4,16 +4,23 @@ const userSchema = mongoose.Schema({
     username : {
         type : String,
         required : true,
-        unique : true
+        unique : true,
+        minLength : [3 , "username must be atleast 3 character"],
+        maxLength : [20 , "username must be most 20 character"]
     },
     email : {
         type : String,
         required : true,
-        unique : true
+        unique : true,
+        minLength : [6 , "email must be atleast 6 character"],
+        maxLength : [50 , "email must be most 50 character"],
+        trim : true
     },
     password : {
         type : String,
-        required : true
+        required : true,
+        select : false
+
     }
 })
 
